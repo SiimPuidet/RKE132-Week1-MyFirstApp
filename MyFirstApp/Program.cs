ab604755f9920d2f56909ab63f542410f9a6c0cf
Siim Puidet <siim.puidet@tktk.ee>
2024-11-07 08:20:07 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+
+// Rakendus palub kasutajal sisestada oma nimi.
+Console.WriteLine("Enter your name:"); //Output
+
+// String on sõne
+string userName = Console.ReadLine(); //Input
+
+// Rakendus tervitab kasutajat nimepidi.
+// Pikk variant
+Console.WriteLine("Hello, " + userName + "!"); //Output
+// Lühike variant - String interpolation
+Console.WriteLine($"Hello, {userName}!"); //Output
